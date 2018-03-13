a27d45642dc80a0f9355777e1a09cc7e0c6e460b
Ken Spencer <me@iotaspencer.me>
2018-03-13 11:01:05 -0400

    add more to the hook source

--- lib/site_hook.rb
+++ lib/site_hook.rb
@@ -1,24 +1,20 @@
+require 'site_hook/version'
+require 'site_hook/sender'
+require 'site_hook/logger'
 require 'sinatra'
 require 'sinatra/json'
 require 'yaml'
-require "site_hook/version"
-require 'site_hook/sender'
+
 module SiteHook
   class Webhook < Sinatra::Base
     set port: 9090
     set bind: '0.0.0.0'
 
     def Webhook.verified?(body, hub_sig, secret)
-      dig = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA1.new, secret, body)
-      puts "body => #{body}"
-      puts "secret => #{secret}"
-      puts "hub_sig => #{hub_sig}"
-      puts "dig => #{dig}"
-      if hub_sig == dig
+      if hub_sig == OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA1.new, secret, body)
         true
       end
     end
-
     get '/' do
       halt 403, {'Content-Type' => 'application/json'}, {message: 'no permission'}.to_json
     end
@@ -29,15 +25,15 @@ module SiteHook
       request.body.rewind
       req_body = request.body.read
       sig      = request.env['HTTP_X_HUB_SIGNATURE'].sub!(/^sha1=/, '')
-      projects = YAML.load_file(Pathname(Dir.home).join('.jph-rc'))['projects']
+      jph_rc = YAML.load_file(Pathname(Dir.home).join('.jph-rc'))
+      projects = jph_rc['projects']
       begin
         project = projects.fetch(params[:hook_name])
       rescue KeyError => e
         halt 404, {'Content-Type' => 'application/json'}, {message: 'no such project', status: 1}.to_json
       end
-
       if Webhook.verified?(req_body.to_s, sig, project['hookpass'])
-        #SiteHook::Sender.send('doodie')
+        jekyllbuild = SiteHook::Senders::JekyllBuild.new(project['src'], project['dst'], logger: settings.hook)
         status 200
         headers 'Content-Type' => 'application/json'
         body {
@@ -49,12 +45,6 @@ module SiteHook
     end
     post '/webhook/?' do
       halt 403, {'Content-Type' => 'application/json'}, {message: 'pick a hook', error: 'root webhook hit'}.to_json
-    end
-  end
-  module CLI
-    def run
-      SiteHook::Webhook.new
-
     end
   end
 end
