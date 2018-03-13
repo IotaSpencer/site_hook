c91196127a681d541db16b069e372859897025be
Ken Spencer <me@iotaspencer.me>
2018-03-13 11:08:14 -0400

    fix chain order on class normalization

--- lib/site_hook/logger.rb
+++ lib/site_hook/logger.rb
@@ -9,19 +9,20 @@ module SiteHook
       @log = Logging.logger[self.class.to_s]
       @log.level = log_level
       Logging.appenders.rolling_file(
-        Pathname(Dir.home).join('.jph', 'logs', "#{self.class.gsub(/::/, '').to_s.downcase}-#{@log.level}.log").to_path,
+        Pathname(Dir.home).join('.jph', 'logs', "#{self.class.to_s.gsub(/::/, '').downcase}-#{@log.level}.log").to_path,
         :age => 'daily',
         :layout => Logging.layouts.pattern(color_scheme: 'default'),
       )
     end
   end
+
   @@ll = LogLogger.new
   @@ll.debug "#{@@ll.class} initialized."
+
   class HookLogger
     Logging.logger.root.appenders = Logging.appenders.stdout
 
     def HookLogger.mklogdir
-      
       path = Pathname(Dir.home).join('.jph', 'logs')
       @@ll.debug("Checking if #{path} exists.")
       if path.exist?
@@ -36,7 +37,7 @@ module SiteHook
     class Hook
       attr :log
       attr :log_level
-      
+
       def initialize(log_level = nil)
         @@ll.debug "Initializing #{self}"
         @log = Logging.logger[self.class.to_s]
