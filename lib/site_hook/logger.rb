d0d2682ea832b5c01694d877aa8f7a5d02f3dc08
Ken Spencer <me@iotaspencer.me>
2018-03-16 16:26:17 -0400

    try this again

--- lib/site_hook/logger.rb
+++ lib/site_hook/logger.rb
@@ -132,9 +132,9 @@ module SiteHook
             msg.squish!
             case
             when msg =~ /From (github|gitlab)\.com:(.+)\/(.+)(\.git)?/
-              @debug_output << "Pulling via $2/$3 on $1."
-            when msg =~ / \* branch (.+).*/
-              @debug_output << 'Using $1 branch'
+              @debug_output << "Pulling via #{$2}/#{$3} on #{$1}."
+            when msg =~ /\* branch (.+).*/
+              @debug_output << "Using #{$1} branch"
             end
           end
         else
