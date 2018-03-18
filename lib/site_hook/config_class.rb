b9b11d69850b4a3a548df2f5742297d0c043b208
Ken Spencer <me@iotaspencer.me>
2018-03-18 10:55:42 -0400

    lets try removing the query

--- lib/site_hook/config_class.rb
+++ lib/site_hook/config_class.rb
@@ -6,13 +6,11 @@ module SiteHook
     YML = RecursiveOpenStruct.new(YAML.load_file(Pathname(Dir.home).join('.jph-rc')))
 
     desc 'list QUERY [options]', 'List configured options'
-    def list(query = nil)
-      case query
-      when '-a', 'all'
-        YML.each do |directive, hsh|
-          say directive
-          say hsh
-        end
+
+    def list
+      YML.each do |directive, hsh|
+        say directive
+        say hsh
       end
     end
   end
