15f915e03b890fabdfee5ac17cffb01ebf9933c9
Ken Spencer <me@iotaspencer.me>
2018-03-22 14:23:40 -0400

    styling

--- lib/site_hook.rb
+++ lib/site_hook.rb
@@ -28,7 +28,7 @@ module SiteHook
     set :views, Pathname(app_file).dirname.join('site_hook', 'static', 'sass').to_s
     get '/css/*.css' do
       filename = params[:splat].first
-      sass filename.to_sym
+      scss filename.to_sym
     end
   end
   class CoffeeHandler < Sinatra::Base
