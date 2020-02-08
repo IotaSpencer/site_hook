0d77a70524375b71bf1aceccf773b1d6cbebeafa
IotaSpencer <me@iotaspencer.me>
2020-02-08 18:48:19 -0500

    do defaults within the method

--- lib/site_hook/commands/server_class.rb
+++ lib/site_hook/commands/server_class.rb
@@ -13,13 +13,22 @@ module SiteHook
       # puts SiteHook::VERSION
       # end
       # map ['-v', '--version'] => __version
-      method_option(:host, banner: 'HOST', aliases: ['-h'], type: :string, default: SiteHook::Config.webhook.host)
-      method_option(:port, banner: 'PORT', aliases: ['-p'], type: :numeric, default: SiteHook::Config.webhook.port)
+      method_option(:host, banner: 'HOST', aliases: ['-h'], type: :string)
+      method_option(:port, banner: 'PORT', aliases: ['-p'], type: :numeric)
       desc 'listen [options]', ''
       def listen
+        host = SiteHook::Config.webhook.host
+        port = SiteHook::Config.webhook.port
+        if options['host']
+          host = options['host']
+        end
+        if options['port']
+          port = options['port']
+        end
+
         $threads << Thread.new do
           PreLogger.debug options
-          ::Thin::Server.start(options['host'], options['port'], SiteHook::Server, debug: true)
+          ::Thin::Server.start(host, port, SiteHook::Server, debug: true)
         end
         $threads << Thread.new do
           loop do
